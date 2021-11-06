cfc2558255003907f129261bf33d4bf82da85721
relaviz <68802672+relaviz@users.noreply.github.com>
2021-11-06 22:05:00 +0200

    Final hw2

---
+++ HW2.js
@@ -0,0 +1,11 @@
+let integer = prompt('Enter integer number:', '3');
+
+const fibonacci = (f0, f1, n) => {
+    if (n === 0 || n === 1) return f0;
+    if (n === 2) return f1;
+    if (n >= 3) {
+        return fibonacci(f0, f1, n - 1) + fibonacci(f0, f1, n - 2);
+    }
+}
+
+document.write( fibonacci(-10, 5, integer))
